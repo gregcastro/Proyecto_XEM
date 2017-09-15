3dd4cf85099e0c7820ccdf2c78de645660a22037
gregcastro <josegregorio994@gmail.com>
2017-09-15 09:25:08 -0400

    Se agrego el repo de python

---
+++ XEM Monitor/app_backend.py
@@ -0,0 +1,210 @@
+import subprocess
+import shutil
+import os
+import requests
+import json
+import socket
+import time
+from multiprocessing import Queue
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+from win32com.client import Dispatch
+import win32com.shell.shell as shell
+import win32event
+from win32com.client import GetObject
+
+# data JSON model (GET request)
+# {
+#   "accion": {
+#     "cambiar_version_claymore": false,
+#     "reiniciar_claymore": false,
+#     "cambiar_start_bat": false,
+#     "descargar_nueva_version_claymore": false,
+#     "resetear_RIG": false
+#   },
+#   "propiedades": {
+#     "version": "nombre de version",
+#     "start_bat_data": "configuracion de claymore",
+#     "IP_Reset": "IP de RIG a resetear"
+#   }
+# }
+
+# data JSON model (GET request)
+# {
+#   "action": {
+#     "action_change_claymore_version": false,
+#     "action_restart_claymore": false,
+#     "action_change_start_bat": false,
+#     "action_download_claymore_version": false,
+#     "resetear_RIG": false
+#   },
+#   "properties": {
+#     "version": "nombre de version",
+#     "start_bat_data": "configuracion de claymore",
+#     "IP_Reset": "IP de RIG a resetear"
+#   }
+# }
+
+target = r"C:\Users\Miner\Miners\Claymore\Claymore's Dual Ethereum+Decred_Siacoin_Lbry_Pascal AMD+NVIDIA GPU Miner v9.5 - Catalyst 15.12-17.x - CUDA 8.0_7.5_6.5\start.bat"
+wDir = r"C:\Users\Miner\Miners\Claymore\Claymore's Dual Ethereum+Decred_Siacoin_Lbry_Pascal AMD+NVIDIA GPU Miner v9.5 - Catalyst 15.12-17.x - CUDA 8.0_7.5_6.5"
+icon = r"C:\Users\Miner\Miners\Claymore\Claymore's Dual Ethereum+Decred_Siacoin_Lbry_Pascal AMD+NVIDIA GPU Miner v9.5 - Catalyst 15.12-17.x - CUDA 8.0_7.5_6.5\start.bat"
+data = json.loads('{"accion": {"cambiar_version_claymore": false,"reiniciar_claymore": false,"cambiar_start_bat": false,"descargar_nueva_version_claymore": false,"resetear_RIG": false},"propiedades": {"version": "version1","start_bat_data": "-wd 1 -r 1 -epool stratum+tcp://eth-us.dwarfpool.com:8008 -ewal 0x67Eb849500f4bf5fd5ac7dD34E65Fa0f02Bd09d9/LETHI19/luisgermim@gmail.com -esm 0 -mode 0 -epsw x -allpools 1 -mport -3333 -dpool stratum+tcp://dcr.suprnova.cc:3252 -dwal luisgermim.LETHI19 -dpsw 1234 -tt -77 -ttdcr 80 -fanmin 80","IP_Reset": "192.168.0.190"}}')
+windows_config = 'setx GPU_FORCE_64BIT_PTR 0\nsetx GPU_MAX_HEAP_SIZE 100\nsetx GPU_USE_SYNC_OBJECTS 1\nsetx GPU_MAX_ALLOC_PERCENT 100\nsetx GPU_SINGLE_ALLOC_PERCENT 100\ntimeout /t 20\nprocess_printer.exe -c "EthDcrMiner64.exe '
+myIP = ''
+# web_server = 'http://172.16.10.103:8081/xem'
+web_server = 'http://192.168.0.100:8081'
+
+def web_get_request_JSON():
+    global data
+    #r = requests.get(web_server + "/rig")
+    r = requests.get('http://192.168.0.100:8081' + '/rig')
+    
+    ##################
+    print(r.status_code)
+    # print(r.headers['content-type'])
+    # print(r.json())
+    ##################
+
+    #hacer r.content da TypeError: Can't convert 'bytes' obj to str implicitly
+    # data = json.loads(r.content)
+
+    #linea agregada por gcastro
+    data = r.json()
+
+    #Print de gcastro
+    print("data = ")
+    print(data)
+
+def obtener_myIP():
+    global myIP
+    hostname = socket.gethostname()
+    myIP = socket.gethostbyname(hostname)
+    print("Mi direccion IP es: ")
+    print(myIP)
+
+def inicializar_cliente_TCP():
+    global address
+    global client_socket
+    address = ('192.168.0.254', 8888) #Define who you are talking to (must match arduino IP and port)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Set Up the Socket
+    client_socket.settimeout(2) #only wait 1 second for a resonse
+
+def crear_acceso_directo_start_bat():
+    path = os.path.join(r"C:\Users\Miner\Miners\Claymore", "start.lnk")
+    shell = Dispatch('WScript.Shell')
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = wDir
+    shortcut.IconLocation = icon
+    shortcut.save()
+
+def iniciar_claymore():
+    se_ret = shell.ShellExecuteEx(fMask=0x140, lpFile=r"C:\Users\Miner\Miners\Claymore\start.lnk", nShow=1)
+    time.sleep(5) #Esperar a que inicie el claymore
+
+def reiniciar_claymore():
+    os.system("TASKKILL /F /T /IM cmd.exe")
+    time.sleep(3)
+    iniciar_claymore()
+
+def request_reiniciar_claymore():
+    if data['accion']['reiniciar_claymore'] == True:
+        reiniciar_claymore()
+        data['accion']['reiniciar_claymore'] = False
+
+def cambiar_version_claymore():
+    global target
+    global wDir
+    global icon
+    if data['accion']['cambiar_version_claymore'] == True:
+        target = "C:\\Users\\Miner\\Miners\\Claymore\\" + data['propiedades']['version'] + "\\start.bat"
+        wDir = "C:\\Users\\Miner\\Miners\\Claymore\\" + data['propiedades']['version']
+        icon = "C:\\Users\\Miner\\Miners\\Claymore\\" + data['propiedades']['version'] + "\\start.bat"
+        if os.path.exists(wDir):
+            crear_acceso_directo_start_bat()
+            reiniciar_claymore()
+        data['accion']['cambiar_version_claymore'] = False
+
+def cambiar_start_bat():
+    if data['accion']['cambiar_start_bat'] == True:
+        if os.path.exists(wDir):
+            with open(target,'w') as f:
+           # with open(r"C:\Users\Miner\Miners\Claymore\start.txt",'w') as f:
+                f.write(windows_config + data['propiedades']['start_bat_data'] + '"')
+            f.closed
+            crear_acceso_directo_start_bat()
+            reiniciar_claymore()
+            data['accion']['cambiar_start_bat'] = False
+
+def descargar_nueva_version_claymore():
+    # if data['accion']['descargar_nueva_version_claymore'] == True:
+    if data['action']['action_download_claymore_version'] == "1":
+        zipurl = web_server + "/data/Claymore.zip"
+        with urlopen(zipurl) as zipresp:
+            zipinfo = zipresp.infolist()
+            filename = zipinfo[0].filename()
+            dest = "C:\\Users\\Miner\\Miners\\Claymore\\" + filename
+            if not os.path.exists(dest):
+                with ZipFile(BytesIO(zipresp.read())) as zipfile:
+                    zipfile.extractall(r"C:\Users\Miner\Miners\Claymore")
+                zipfile.close()
+#                src = r"C:\Users\Miner\Miners\Claymore\winpty_cygwin_process_printer"
+                src = os.path.dirname(os.path.realpath(__file__)) + r"\winpty_cygwin_process_printer"
+                src_files = os.listdir(src)
+                for file_name in src_files:
+                    full_file_name = os.path.join(src, file_name)
+                    if os.path.is_file(full_file_name):
+                        shutil.copy(full_file_name, dest)
+                target = dest + "\\start.bat"
+                wDir = dest
+                icon = dest + "\\start.bat"
+                crear_acceso_directo_start_bat()
+                reiniciar_claymore()
+        # data['accion']['descargar_nueva_version_claymore'] = False
+        data['action']['action_download_claymore_version'] = "0"
+
+def resetear_RIG():
+    global client_socket
+    if data['accion']['resetear_RIG'] == True and data['propiedades']['IP_Reset'] == myIP:
+        client_socket.sendto(bytes("1", "utf-8"), address)
+        print("Paquete enviado")
+        data['accion']['resetear_RIG'] = False
+
+def setup_winpty_cygwin_process_printer():
+    dest = "C:\\Users\\Miner\\Miners\\Claymore"
+    dest_directories = os.listdir(dest)
+    src = os.path.dirname(os.path.realpath(__file__)) + r"\winpty_cygwin_process_printer"
+    src_files = os.listdir(src)
+    for directory_name in dest_directories:
+        full_directory_name = os.path.join(dest, directory_name)
+        for file_name in src_files:
+            full_file_name = os.path.join(src, file_name)
+            if os.path.isfile(full_file_name) and os.path.isdir(full_directory_name):
+                shutil.copy(full_file_name, full_directory_name)
+
+def main():
+    setup_winpty_cygwin_process_printer()
+    obtener_myIP()
+    inicializar_cliente_TCP()
+    iniciar_claymore()
+
+    while True:
+        try:
+            web_get_request_JSON()
+            time.sleep(2)
+            print('gcastro: after time sleep')
+        except:
+            print('ERROR GET')
+            time.sleep(2) # Reintentar en 1 segundo
+            pass
+        else:
+            print("descargar_nueva_version_claymore")
+            descargar_nueva_version_claymore()
+            print("FIN")
+            # cambiar_start_bat()
+            # cambiar_version_claymore()
+            # request_reiniciar_claymore()
+            # resetear_RIG()
+
+main()
---
+++ XEM Monitor/winpty_cygwin_process_printer/process_printer.py
@@ -0,0 +1,250 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import json
+from subprocess import Popen, PIPE
+import time
+import datetime
+import socket
+import requests
+import optparse
+from multiprocessing import Queue
+
+
+# Configuration variables
+seconds_between_requests = 1
+# web_server_address = 'http://192.168.0.102:8081/xem'
+web_server_address = 'http://192.168.0.100:8081'
+# payload informatcion
+user_email = "alfonsof@gmail.com"
+rig_name = "Min02"
+claymore_version = "9.5"
+
+
+
+# Parse options from outside to retrieve Miner command
+parser = optparse.OptionParser('usage: python process_printer '+ '-c <Miner Command>')
+parser.add_option('-c', dest='command', type='str', help='Miner Command')
+parser.add_option('-r', dest='rig_reset_today', type='str', help='Numbers of resets today')
+parser.add_option('-t', dest='rig_claymore_reset_today', type='str', help='Numbers of Claymore resets today ?')
+(options, args) = parser.parse_args()
+
+# If applicable, fill dict values
+if (options.rig_reset_today != None):
+    gpu_info["rig_reset_today"] = options.rig_reset_today
+
+    if (options.rig_claymore_reset_today != None):
+        gpu_info["rig_claymore_reset_today"] = options.rig_claymore_reset_today
+
+
+
+
+# Global dictionary
+gpu_info = {
+"rig_email":"None",
+"rig_name":"None",
+"rig_gpu_second_coin":"None",
+"rig_lan_ip":"None",
+"rig_claymore_version":"None",
+"rig_time_up":"None",
+"rig_reset_today":"None",
+"location_uuid":"None",
+"rig_claymore_reset_today":"None",
+"rig_gpu_info_eth": [
+ {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"},
+ {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"},
+ {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"},
+ {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"},
+ {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"},
+ {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"},
+ {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"},
+ {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"}
+    ],
+"rig_gpu_info_second_coin": [
+{"Mhs":"None"},
+{"Mhs":"None"},
+{"Mhs":"None"},
+{"Mhs":"None"},
+{"Mhs":"None"},
+{"Mhs":"None"},
+{"Mhs":"None"},
+{"Mhs":"None"}
+    ]
+}
+
+#'gpu_info = {"rig_email":"None", "rig_name":"None", "rig_gpu_second_coin":"None", "rig_lan_ip":"None", "rig_claymore_version":"None", "rig_time_up":"None", "rig_reset_today":"None", "rig_claymore_reset_today":"None", "rig_gpu_info_eth": [{"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"}, {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"}, {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"}, {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"}, {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"}, {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"}, {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"}, {"Mhs":"None", "Temp":"None", "Fan":"None","Model":"None"} ], "rig_gpu_info_second_coin": [{"Mhs":"None"}, {"Mhs":"None"}, {"Mhs":"None"}, {"Mhs":"None"}, {"Mhs":"None"}, {"Mhs":"None"}, {"Mhs":"None"}, {"Mhs":"None"} ] }'
+
+
+### Global dictionary Example
+# gpu_info = {\
+# "rig_email":"ljbello@gmail.com",\
+# "rig_name":"Min01",\
+# "rig_gpu_second_coin":"PascalCoin",\
+# "rig_lan_ip":"192.168.1.68",\
+# "rig_claymore_version":"9.5",\
+# "rig_time_up":"09:09",\
+# "RigResetToday":"4",\
+# "rig_claymore_reset_today":"0",\
+# "rig_gpu_info_eth": [\
+# {"Mhs":"24.3", "Temp":"70", "Fan":"80","Model":"Rx480"},\
+# {"Mhs":"24.1", "Temp":"68", "Fan":"80","Model":"Rx480"},\
+# {"Mhs":"24.5", "Temp":"68", "Fan":"90","Model":"Rx480"},\
+# {"Mhs":"24.1", "Temp":"73", "Fan":"80","Model":"Rx480"},\
+# {"Mhs":"24.7", "Temp":"76", "Fan":"80","Model":"Rx480"}\
+#    ],\
+# "rig_gpu_info_second_coin": [\
+# {"Mhs":"2434.3" },\
+# {"Mhs":"2422.1"},\
+# {"Mhs":"2433.5"},\
+# {"Mhs":"2433.1"},\
+# {"Mhs":"2411.7"}\
+#    ]\
+# }
+
+# Check Local Ip Address
+gpu_info["rig_lan_ip"] = socket.gethostbyname(socket.gethostname())
+gpu_info["rig_time_up"] = '0'
+
+# Time Keeper
+start_time = time.time()
+tick_time = start_time
+
+############################################ PARSER ############################################
+#
+# Here in lies the parsing logic, each line that the mining program prints passes through here to be examined for usefull information.
+def parse_line(line,dict_output):
+
+    if 'ETH' in line:
+        #Check if the GPU keyword is in the line.
+        if 'GPU' in line:
+            #CHeck if it's the line with no information
+            if 'GPU ' not in line and 'GPU_' not in line:
+
+                #Check which GPU we are talking about
+                gpu_number = int(line[ line.find('GPU') + 3])
+
+                # Find out what information is provided by th line
+                if 'Mh/s' in line:
+                    CpuSpeed = float(line[ line.find('GPU' + str(gpu_number)) + 4: line.find('Mh/s')].strip())
+                    dict_output["rig_gpu_info_eth"][gpu_number]['Mhs'] = CpuSpeed
+
+                    if 't=' in line:
+                        Temp = int(line[ line.find('t=') + 2: line.find('C')])
+                        dict_output["rig_gpu_info_eth"][gpu_number]['Temp'] = Temp
+
+                        fan = int(line[ line.find('fan=') + 4: line.find('%')])
+                        dict_output["rig_gpu_info_eth"][gpu_number]['Fan'] = fan
+
+    if 'DCR' in line:
+        # Update the status of the second coin
+        dict_output["rig_gpu_second_coin"] = "Decred"
+
+        # CHeck if we got GPU information
+        if 'GPU' in line:
+            #CHeck if it's the line with no information
+            if 'GPU ' not in line and 'GPU_' not in line:
+
+                #Check which GPU we are talking about
+                gpu_number = int(line[ line.find('GPU') + 3])
+
+                # Find out what information is provided by th line
+                if 'Mh/s' in line:
+                    CpuSpeed = float(line[ line.find('GPU' + str(gpu_number)) + 4: line.find('Mh/s')].strip())
+                    dict_output["rig_gpu_info_second_coin"][gpu_number]['Mhs'] = CpuSpeed
+
+
+    if 'GPU' in line and 'ETH' not in line and 'DCR' not in line:
+        # Process the line that contains the temperature
+        if 'GPU ' not in line and 'GPU_' not in line:
+
+            if 't=' in line:
+                #Check which GPU we are talking about
+                gpu_number = int(line[ line.find('GPU') + 3])
+
+                # Parse Temperature and fan values
+                Temp = int(line[ line.find('t=') + 2: line.find('C')])
+                dict_output["rig_gpu_info_eth"][gpu_number]['Temp'] = Temp
+
+                fan = int(line[ line.find('fan=') + 4: line.find('%')])
+                dict_output["rig_gpu_info_eth"][gpu_number]['Fan'] = fan
+
+
+    # This is particularly to check which GPU es currently running:
+    if "recognized as" in line:
+
+        print("line = ")
+        print(line)
+        #Check which GPU we are talking about
+        gpu_number = int(line[ line.find('recognized as') - 2])
+        print("gpu_number = ")
+        print(gpu_number)
+        #Parse GPU Model
+        model = line[ line.find('recognized as') + 14:]
+        print("model = ")
+        print(model)
+        dict_output["rig_gpu_info_eth"][gpu_number]['Model'] = model
+
+def inicializar_cliente_TCP():
+    global address
+    global client_socket
+    address = ('192.168.0.254', 8888) #Define who you are talking to (must match arduino IP and port)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Set Up the Socket
+    client_socket.settimeout(2) #only wait 1 second for a resonse
+
+def pedir_num_resets():
+    global client_socket
+    client_socket.sendto(bytes("0", "utf-8"), address)
+    try:
+        rec_data, addr = client_socket.recvfrom(2048) #Read response from arduino
+        print('El numero de resets de este equipo es: ' + rec_data) #Print the response from Arduino
+        gpu_info["rig_reset_today"] = rec_data
+    except:
+        pass
+
+######################################################### MAIN ###################################################################
+inicializar_cliente_TCP()
+pedir_num_resets()
+with Popen(r"winpty.exe -Xallow-non-tty -Xplain ./" + options.command, stdout=PIPE, bufsize=1, shell=True) as p:
+    for line in p.stdout:
+        # This is the main loop
+
+        # Encode line as utf-8
+        line = str(line,'utf-8')
+
+        # Run the Parsing logic
+        parse_line(line,gpu_info)
+
+        # check if 10 seconds have passed since last sent request
+        if time.time() - tick_time > seconds_between_requests:
+            # Send request
+            # Fill variables that are not parsed from the console.
+            gpu_info["rig_email"] = user_email
+            gpu_info["rig_name"] = rig_name
+            gpu_info["rig_claymore_version"] = claymore_version
+            # calculate elapsed time.
+            gpu_info["rig_time_up"] = "{}".format(datetime.timedelta(seconds=int(time.time() - start_time)))
+
+
+            GPUInfoETH_json = gpu_info["rig_gpu_info_eth"]
+            GPUInfoSecondCoin_json = gpu_info["rig_gpu_info_second_coin"]
+
+            print('\n\n\n##################################')
+            # print(GPUInfoETH_json)
+            # print(GPUInfoSecondCoin_json)
+            print(gpu_info)
+            print('##################################\n\n\n')
+
+            # Send request to server
+            try:
+
+                requests.post(web_server_address+'/rig', data={"gpu_info":str(gpu_info)})
+                # requests.post(web_server_address+'/rig', data={"gpu_info":str(gpu_info), "rig_gpu_info_eth_json":str(GPUInfoETH_json), "rig_gpu_info_second_coin_json":str(GPUInfoSecondCoin_json) })
+                # requests.post('http://192.168.2.103/xem/rig', data=gpu_info_string)
+            except:
+                print('ERROR POST')
+                pass
+            #print information to console
+            print(gpu_info)
+            print(line)
+            tick_time = time.time()
